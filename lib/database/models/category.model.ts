4de5d0687b12720765765a5240e2015ddb855d51
Yann6666 <yann.cotard01@gmail.com>
2024-01-30 00:22:04 +0100

    Création du CRUD

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,15 @@
+import { models } from "mongoose";
+import { Document, Schema, model } from "mongoose";
+
+export interface ICategory extends Document {
+    _id: string;
+    name: string;
+}
+
+const CategorySchema = new Schema({
+    name: { type: String, required: true, unique: true },
+})
+
+const Category = models.Category || model('Category', CategorySchema)
+
+export default Category;
